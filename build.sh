6f9db99c93fae10d347ec7bb65db0af33d1224f3
medsouz <medsouz99@gmail.com>
2013-09-19 21:00:50 -0400

    Fixed build script, made travis download wine
    I'm not that good at this. :D

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 python setup.py
-cd forge/mcp/
-python runtime/recompile.py "$@"
 rc=$?
 if [[ $rc != 0 ]] ; then
     exit $rc
 fi
-python runtime/reobfuscate.py "$@"
+cd forge/mcp/
+python runtime/recompile.py
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+python runtime/reobfuscate.py
 rc=$?
 if [[ $rc != 0 ]] ; then
     exit $rc
