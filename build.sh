60ba113cf21d729cd976fa4008c1659929ea51a3
medsouz <medsouz99@gmail.com>
2013-09-19 20:22:33 -0400

    Build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+python setup.py
+cd forge/mcp/
+python runtime/recompile.py "$@"
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+python runtime/reobfuscate.py "$@"
+rc=$?
+if [[ $rc != 0 ]] ; then
+    exit $rc
+fi
+echo "OpenModularGun has compiled successfully!";
